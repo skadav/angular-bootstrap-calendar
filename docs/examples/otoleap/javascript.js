9515a7034c4538a934c1087e75aec46b7f48d0f1
skadav <satu.banyumas@gmail.com>
2021-03-23 12:11:40 +0700

    Add files via upload
    fix allow multiple event

--- docs/examples/otoleap/javascript.js
+++ docs/examples/otoleap/javascript.js
@@ -8,32 +8,33 @@ angular
     vm.endTime = "18:00";
     //Event Items
     vm.events = [
-      {
-        title: 'JUMAIL',
-        color: calendarConfig.colorTypes.warning,
-        startsAt: moment(moment().startOf('month').toDate()).add(12, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(14, 'hour'),
-        draggable: true,
-        column: 1,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      },
+      // {
+      //   title: 'JUMAIL',
+      //   color: calendarConfig.colorTypes.warning,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(12, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(14, 'hour'),
+      //   draggable: true,
+      //   column: 1,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // },
       {
         title: 'Service 1',
         color: calendarConfig.colorTypes.warning,
         startsAt: moment(moment().startOf('month').toDate()).add(15, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(17, 'hour'),
+        endsAt: moment(moment().startOf('month').toDate()).add(16, 'hour'),
         draggable: true,
-        column: 1,
+        width: 150,
+        column: 0,
         actions: [{
           label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
           onClick: function (args) {
@@ -52,7 +53,8 @@ angular
         startsAt: moment(moment().startOf('month').toDate()).add(15, 'hour'),
         endsAt: moment(moment().startOf('month').toDate()).add(16, 'hour'),
         draggable: true,
-        column: 1,
+        column: 0,
+        width: 150,
         actions: [{
           label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
           onClick: function (args) {
@@ -68,10 +70,11 @@ angular
       {
         title: 'Service 3',
         color: calendarConfig.colorTypes.warning,
-        startsAt: moment(moment().startOf('month').toDate()).add(14, 'hour'),
+        startsAt: moment(moment().startOf('month').toDate()).add(15, 'hour'),
         endsAt: moment(moment().startOf('month').toDate()).add(16, 'hour'),
         draggable: true,
         column: 1,
+        width: 300,
         actions: [{
           label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
           onClick: function (args) {
@@ -84,149 +87,149 @@ angular
           }
         }]
       },
-      {
-        title: 'double 1',
-        code: 'Service Code',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(9, 'hour'),
-        draggable: true,
-        column: 0,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      },
-      {
-        title: 'double 2',
-        code: 'Service Code  swsw',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment(moment().startOf('month').toDate()).add(8.5, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
-        draggable: true,
-        column: 0,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      },
-      {
-        title: 'double 3',
-        code: 'Service Code  swsw',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
-        draggable: true,
-        column: 0,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      },
-      {
-        title: 'double 4',
-        code: 'Service Code  swsw',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(9, 'hour'),
-        draggable: true,
-        column: 0,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      },
-      {
-        title: 'double 5',
-        code: 'Service Code  swsw',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment(moment().startOf('month').toDate()).add(7.5, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
-        draggable: true,
-        column: 0,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      },
-      {
-        title: 'double 6',
-        code: 'Service Code  swsw',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
-        draggable: true,
-        column: 0,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      }, {
-        title: 'double 7',
-        code: 'Service Code  swsw',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
-        endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
-        draggable: true,
-        column: 0,
-        actions: [{
-          label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-          onClick: function (args) {
-            alert.show('Edited', args.calendarEvent);
-          }
-        }, {
-          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-          onClick: function (args) {
-            alert.show('Deleted', args.calendarEvent);
-          }
-        }]
-      }
+      // {
+      //   title: 'double 1',
+      //   code: 'Service Code',
+      //   color: calendarConfig.colorTypes.info,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(9, 'hour'),
+      //   draggable: true,
+      //   column: 0,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // },
+      // {
+      //   title: 'double 2',
+      //   code: 'Service Code  swsw',
+      //   color: calendarConfig.colorTypes.info,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(8.5, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
+      //   draggable: true,
+      //   column: 0,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // },
+      // {
+      //   title: 'double 3',
+      //   code: 'Service Code  swsw',
+      //   color: calendarConfig.colorTypes.info,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
+      //   draggable: true,
+      //   column: 0,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // },
+      // {
+      //   title: 'double 4',
+      //   code: 'Service Code  swsw',
+      //   color: calendarConfig.colorTypes.info,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(9, 'hour'),
+      //   draggable: true,
+      //   column: 0,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // },
+      // {
+      //   title: 'double 5',
+      //   code: 'Service Code  swsw',
+      //   color: calendarConfig.colorTypes.info,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(7.5, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
+      //   draggable: true,
+      //   column: 0,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // },
+      // {
+      //   title: 'double 6',
+      //   code: 'Service Code  swsw',
+      //   color: calendarConfig.colorTypes.info,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
+      //   draggable: true,
+      //   column: 0,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // }, {
+      //   title: 'double 7',
+      //   code: 'Service Code  swsw',
+      //   color: calendarConfig.colorTypes.info,
+      //   startsAt: moment(moment().startOf('month').toDate()).add(8, 'hour'),
+      //   endsAt: moment(moment().startOf('month').toDate()).add(10, 'hour'),
+      //   draggable: true,
+      //   column: 0,
+      //   actions: [{
+      //     label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Edited', args.calendarEvent);
+      //     }
+      //   }, {
+      //     label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+      //     onClick: function (args) {
+      //       alert.show('Deleted', args.calendarEvent);
+      //     }
+      //   }]
+      // }
     ];
 
 
-    vm.columns = [1, 2, 3, 4, 5];
+    vm.columns = [1,2];
     vm.calendarView = 'day';
     vm.viewDate = moment().startOf('month').toDate();
     vm.cellIsOpen = true;
